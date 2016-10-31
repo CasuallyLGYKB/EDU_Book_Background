9f448816e1e6b0523704e6d9b2f0b4b0656e6063
lgybetter <437675103@qq.com>
2016-10-31 11:22:05 +0800

    Debug the find books operator

--- controllers/findMsgController.js
+++ controllers/findMsgController.js
@@ -1,24 +1,26 @@
 import UserModel from '../models/user'
 import BookModel from '../models/book.js'
 import querystring from 'querystring'
+import _ from 'underscore'
 
 exports.findAllBookGet = async (ctx, next) => {
   /**
    * 排序api:
-   * localhost:3000/find/all?school=华南师范大学&order=price_des
+   * localhost:3000/find/all?swapMode=快递&price=20&order=price_des
    * order=
    *  1.price_des(降序) price_asc(升序)
    *  2.createdTime_des(降序) createdTime_asc(升序)
    *  3.appearanceLevel_des(新旧程度降序) appearanceLevel_asc(新旧程度升序)
    */
-  var query = ctx.request.query,
-    condition = query.order.split('_');
-  var sortType = condition[1] == 'asc' ? 1 : -1;
-  var conditions = querystring.parse(condition[0]+'='+sortType);
+  var query = _.omit(ctx.request.query,'order'),
+   condition = _.pick(ctx.request.query,'order').order.split('_'), 
+   sortType = condition[1] == 'asc' ? 1 : -1, 
+   conditions = querystring.parse(condition[0] + '=' + sortType);
+  console.log(query);
   console.log(conditions);
-  var books = await BookModel.find(query).catch(e => console.log(e));
+  var books = await BookModel.find(query).sort(conditions).catch(e => console.log(e));
   console.log(books);
-  if(!books.length){
+  if (books.length) {
     ctx.response.body = books;
     console.log("查找书籍成功！");
   } else {
